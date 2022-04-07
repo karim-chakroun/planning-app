cba2782bffd2b40972747e0ac2366fea0e4a8080
karim369 <karim.chakroun@outlook.com>
2022-04-07 14:59:03 +0200

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'planning-app';
+  workingHours: any[] = []
+  myForm: FormGroup;  
+
+  constructor(private fb:FormBuilder) { 
+
+    this.myForm = this.fb.group({  
+      name: '',  
+      quantities: this.fb.array([]) ,  
+    });  
+  }
+
+  quantities() : FormArray {  
+    return this.myForm.get("quantities") as FormArray  
+  } 
+
+  newQuantity(): FormGroup {  
+    return this.fb.group({  
+      qty: '',  
+      price: '',  
+    })  
+  }
+  
+  addQuantity(index:any) {  
+    
+    this.workingHours[index].schedule.push({startTime:'',endTime:''});  
+  }
+
+  
+
+  removeQuantity(i:number,ind:number) {  
+    this.workingHours[i].schedule.pop(ind)  
+  }
+
+  onAdding() {  
+    console.log(this.myForm.value);  
+  } 
+
+
+  ngOnInit() {
+    this.workingHours = [{
+      day: "monday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "tuesday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "wednesday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "thursday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "friday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "saturday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }, {
+      day: "sunday",
+      schedule: [{ startTime: '', endTime: '' }]
+    }]
+  }
+
+  onSubmit() {
+    console.log(this.workingHours)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
